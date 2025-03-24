44e3192f02da81403c023a531e653abbb2468824
Genesius-Julian-Muljadi <juliangenesiusmuljadi@gmail.com>
2025-03-24 23:08:16 +0700

    removed unnecessary redux code

--- apps/web/src/redux/store.ts
+++ apps/web/src/redux/store.ts
@@ -1,20 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import TAPSlice from "./slices/toggleAddProduct";
-import TAMPSlice from "./slices/toggleAddMainProduct";
-import TEPSlice from "./slices/toggleEditProduct";
-import TEMPSlice from "./slices/toggleEditMainProduct";
-import UCSlice from "./slices/updateCookie";
-import UDPSlice from "./slices/updateDashboardProduct";
-import PDPSlice from "./slices/promoteDemoteProduct";
 
 export const store = configureStore({
-  reducer: {
-    TAPSlice,
-    TAMPSlice,
-    TEPSlice,
-    TEMPSlice,
-    UCSlice,
-    UDPSlice,
-    PDPSlice,
-  },
+  reducer: {},
 });
